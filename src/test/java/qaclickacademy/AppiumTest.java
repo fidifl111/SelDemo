7d5845f8f8933d0004572319b9fb25b577fd3490
fidifl111 <fidifl1967@gmail.com>
2019-10-01 18:15:38 -0400

    demo commit of maven test project

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void nativaAppAndroid() {
+		System.out.println("nativaAppAndroid");
+
+	}
+
+	@Test
+	public void iOSApps() {
+		System.out.println("iOSApps");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/REST_API_Test.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class REST_API_Test {
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+
+	}
+
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+
+	}
+
+}
