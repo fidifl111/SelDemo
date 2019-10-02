3059c13881579b10d25a085c6c1920ecadbedfe9
fidifl111 <fidifl1967@gmail.com>
2019-10-01 20:31:33 -0400

    upload after change

--- src/test/java/qaclickacademy/REST_API_Test.java
+++ src/test/java/qaclickacademy/REST_API_Test.java
@@ -6,12 +6,14 @@ public class REST_API_Test {
 	@Test
 	public void postJira() {
 		System.out.println("postJira");
+		System.out.println("IGNORE====Updated for new push to git");
 
 	}
 
 	@Test
 	public void deleteTwitter() {
 		System.out.println("deleteTwitter");
+		System.out.println("IGNORE====Updated for new push to git");
 
 	}
 
