c75f1b0c9fcc184b35ab0177c2188073949182be
fidifl111 <fidifl1967@gmail.com>
2019-10-01 20:53:02 -0400

    3rd commit

--- src/test/java/qaclickacademy/REST_API_Test.java
+++ src/test/java/qaclickacademy/REST_API_Test.java
@@ -6,14 +6,14 @@ public class REST_API_Test {
 	@Test
 	public void postJira() {
 		System.out.println("postJira");
-		System.out.println("IGNORE====Updated for new push to git");
+		System.out.println("IGNORE====Updated for new push to git second time");
 
 	}
 
 	@Test
 	public void deleteTwitter() {
 		System.out.println("deleteTwitter");
-		System.out.println("IGNORE====Updated for new push to git");
+		System.out.println("IGNORE====Updated for new push to git second time");
 
 	}
 
